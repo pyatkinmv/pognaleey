9f065f63d8b620cfa4c4371e68f36a4a241108f8
maxim <pyatkinmv@yandex.ru>
2024-11-26 17:37:13 +0300

    init

---
+++ where-to-go/src/main/java/ru/pyatkinmv/where_to_go/WhereToGoApplication.java
@@ -0,0 +1,13 @@
+package ru.pyatkinmv.where_to_go;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhereToGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhereToGoApplication.class, args);
+	}
+
+}
---
+++ where-to-go/src/test/java/ru/pyatkinmv/where_to_go/WhereToGoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.pyatkinmv.where_to_go;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhereToGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
