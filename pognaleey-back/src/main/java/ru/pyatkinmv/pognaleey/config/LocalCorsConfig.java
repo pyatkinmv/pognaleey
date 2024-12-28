3a82e6b992bf7db9cd2007487ae34156154bf39d
maxim <pyatkinmv@gmail.com>
2024-12-29 02:29:15 +0300

    Configure CORS

--- pognaleey-back/src/main/java/ru/pyatkinmv/pognaleey/config/LocalCorsConfig.java
+++ pognaleey-back/src/main/java/ru/pyatkinmv/pognaleey/config/LocalCorsConfig.java
@@ -7,16 +7,31 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@Profile("local")
 public class LocalCorsConfig {
 
+    @Profile("local")
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
+    public WebMvcConfigurer localCorsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+
+    @Profile("dev")
+    @Bean
+    public WebMvcConfigurer devCorsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://pognaleey.ru")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
