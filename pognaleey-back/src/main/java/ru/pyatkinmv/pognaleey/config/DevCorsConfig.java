402036223f1784a4c5a797fca750b44e2b533d39
maxim <pyatkinmv@gmail.com>
2024-12-29 22:35:22 +0300

    Fix configs; extract secret properties outside

---
+++ pognaleey-back/src/main/java/ru/pyatkinmv/pognaleey/config/DevCorsConfig.java
@@ -0,0 +1,31 @@
+package ru.pyatkinmv.pognaleey.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Profile("dev")
+@Configuration
+public class DevCorsConfig {
+
+    @Value("${cors.allowed-origins}")
+    private String[] corsAllowedOrigins;
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@SuppressWarnings("NullableProblems") CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(corsAllowedOrigins)
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
+
