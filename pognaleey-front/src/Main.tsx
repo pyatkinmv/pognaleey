5699509867e619f87042545ffc204f6b6a9179c7
maxim <pyatkinmv@gmail.com>
2025-01-15 18:10:42 +0300

    Implement tile base

--- pognaleey-front/src/Main.tsx
+++ pognaleey-front/src/Main.tsx
@@ -1,15 +1,47 @@
-// Main.tsx
 import React from "react";
 import "./Main.css";
-import {useNavigate} from "react-router-dom"; // Для перехода между страницами
-
+import {useNavigate} from "react-router-dom";
 
 const Main: React.FC = () => {
-    const navigate = useNavigate(); // Для редиректа
+    const navigate = useNavigate();
 
     const handleButtonClick = () => {
-        navigate(`/travel-inquiries`)
+        navigate(`/travel-inquiries`);
     };
+
+    // Пример данных для плитки
+    const tiles = [
+        {
+            title: "Путешествие в Париж",
+            imageUrl: "/logo192.png", // Замените на путь к вашей картинке
+            text: "Посетите Эйфелеву башню и наслаждайтесь французской кухней.",
+            likes: 120,
+        },
+        {
+            title: "Горы Саян",
+            imageUrl: "/logo192.png",
+            text: "Погрузитесь в атмосферу сибирской природы.",
+            likes: 95,
+        },
+        {
+            title: "Мальдивы",
+            imageUrl: "/logo192.png",
+            text: "Райские пляжи и бирюзовые воды.",
+            likes: 200,
+        },
+        {
+            title: "Амстердам",
+            imageUrl: "/logo192.png",
+            text: "Каналы, музеи и уютные улочки.",
+            likes: 180,
+        },
+        {
+            title: "Япония",
+            imageUrl: "/logo192.png",
+            text: "Культура и природа Киото",
+            likes: 201,
+        }
+    ];
 
     return (
         <div className="main-container">
@@ -24,7 +56,7 @@ const Main: React.FC = () => {
                 </header>
                 <div className="image-container">
                     <img
-                        src="/main.webp" // Замените на путь к вашей картинке
+                        src="/main.webp"
                         alt="Main Banner"
                         className="banner-image"
                     />
@@ -32,6 +64,17 @@ const Main: React.FC = () => {
                         Каждое путешествие<br/>начинается с идеи!
                     </div>
                     <button className="action-button" onClick={handleButtonClick}>Погнали!</button>
+                </div>
+                {/* Плитка */}
+                <div className="tile-container">
+                    {tiles.map((tile, index) => (
+                        <div className="tile" key={index}>
+                            <img src={tile.imageUrl} alt={tile.title} className="tile-image"/>
+                            <h3 className="tile-title">{tile.title}</h3>
+                            <p className="tile-text">{tile.text}</p>
+                            <div className="tile-likes">❤️ {tile.likes}</div>
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
